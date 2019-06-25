523691e9eb834efee7b8ef493638af39ab9e1d91
Jyothi526 <48669246+Jyothi526@users.noreply.github.com>
2019-06-25 16:12:23 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -9,7 +9,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
 	event.respondWith(caches.match(event.request).then((res) => {
 		return res || fetch(event.request).then((response) => {
-			caches.open("Example").then((cache) => {
+			return caches.open("Example").then((cache) => {
 				cache.put(event.request, response.clone());
 				return response;
 			})
