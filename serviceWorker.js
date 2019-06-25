ea86f5c564660d94decbf00e60f20dec2740bd0c
Jyothi526 <48669246+Jyothi526@users.noreply.github.com>
2019-06-25 16:22:54 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,6 @@
 //Installation
 self.addEventListener("install", (event) => {
-	event.waitUntil(caches.open("Example").then((cache) => {
+	event.waitUntil(caches.open("restaurant").then((cache) => {
 		return cache.addAll([
 		]);
 	}))
@@ -9,7 +9,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
 	event.respondWith(caches.match(event.request).then((res) => {
 		return res || fetch(event.request).then((response) => {
-			return caches.open("Example").then((cache) => {
+			return caches.open("restaurant").then((cache) => {
 				cache.put(event.request, response.clone());
 				return response;
 			})
