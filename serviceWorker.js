dc18becbc4ba053e7b08f8f5b36550864e831291
Jyothi <avvarijyothi21@gmail.com>
2019-06-25 15:17:34 +0530

    FEND Restaurant Review

---
+++ serviceWorker.js
@@ -0,0 +1,17 @@
+//Installation
+self.addEventListener("install", (event) => {
+	event.waitUntil(caches.open("Example").then((cache) => {
+		return cache.addAll([]);
+	}))
+})
+// fetch
+self.addEventListener("fetch", (event) => {
+	event.respondWith(caches.match(event.request).then((res) => {
+		return res || fetch(event.request).then((response) => {
+			caches.open("Example").then((cache) => {
+				cache.put(event.request, response.clone());
+				return response;
+			})
+		})
+	}))
+})
